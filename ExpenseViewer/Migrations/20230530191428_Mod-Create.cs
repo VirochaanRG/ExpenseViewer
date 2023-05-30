cf3b5589a9a78c8a1019ddf146290dd73abb618c
Virochaan <39714123+VirochaanRG@users.noreply.github.com>
2023-05-30 15:36:33 -0400

    Add project files.

---
+++ ExpenseViewer/Migrations/20230530191428_Mod-Create.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseViewer.Migrations
+{
+    public partial class ModCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
