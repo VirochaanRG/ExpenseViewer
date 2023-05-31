05ab9b3004ca923fba70650239b15784e0517462
Virochaan <39714123+VirochaanRG@users.noreply.github.com>
2023-05-31 10:50:35 -0400

    Adding Transaction Views

--- ExpenseViewer/Models/Category.cs
+++ ExpenseViewer/Models/Category.cs
@@ -14,7 +14,14 @@ namespace ExpenseViewer.Models
         public string Icon { get; set; } = "";
         [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; } = "Expense";
-
+        [NotMapped]
+        public string? TitleWithIcon
+        {
+            get
+            {
+                return this.Icon + " " + this.Title;
+            }
+        }
 
     }
 }
